002c862aa57614dc095da86e793ef97f75f786ac
MarcGriese <marcgriese76@gmail.com>
2023-12-12 11:44:05 +0100

    created live dates section

---
+++ au_ra/src/components/live-dates.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function LiveDates() {
+    return (
+        <div className="live-dates--container row">
+            {/* <div className="live-dates-header--container col-12">
+                <h1>
+
+                </h1>
+            </div> */}
+            <div className="lives-dates-information--container col-9">
+                <p>
+                    No upcoming tour dates for Au/Ra. Get the newsletter for updates on live gig information, shows on sale, and concert ticket pre-sales.
+                </p>
+            </div>
+            <button className="release-button">
+                <h3>
+                    Sign Me Up!
+                </h3>
+            </button>
+        </div>
+    )
+}
