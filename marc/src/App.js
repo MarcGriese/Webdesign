e1f8743eef821af2c4e6d5ee91d1f6a19e577733
MarcGriese <marcgriese76@gmail.com>
2025-04-03 21:08:26 +0200

    --updated routing

--- marc/src/App.js
+++ marc/src/App.js
@@ -8,21 +8,35 @@ import "@fontsource/ubuntu/400.css"; // Specify weight
 import "@fontsource/ubuntu/latin-400.css"; // Specify weight and style
 import ScrollToTop from './components/ScrollToTop';
 import Projects from './pages/projects';
+import AGBDE from './pages/agbs';
+import DatenschutzDE from './pages/datenschutzerklärung';
 import ImpressumDE from './pages/impressum';
+import TermsAndConditions from './pages/termsAndConditions';
+import LegalNotice from './pages/legalNotice';
+import PrivacyPolicy from './pages/privacyPolicy';
 
 const App = () => {
     return (
         <>
             <ScrollToTop />
             <Routes>
-                <Route  path='/' element={<Navigate to='de' replace />} />
+                <Route path='/' element={<Navigate to='de' replace />} />
                 <Route path='de' element={<HomeDE />} />
                 <Route path='en' element={<HomeEN />} />
 
                 <Route path='*' element={<Navigate to='de' replace />} />
-                <Route path='impressum' element={<ImpressumDE/>}/>
 
-                <Route path='test' element={<Projects/>}/>
+                <Route path='agb' element={<AGBDE />} />
+                <Route path='datenschutz' element={<DatenschutzDE />} />
+                <Route path='impressum' element={<ImpressumDE />} />
+
+                <Route path='termsandconditions' element={<TermsAndConditions />} />
+                <Route path='privacypolicy' element={<PrivacyPolicy />} />
+                <Route path='legalnotice' element={<LegalNotice />} />
+
+
+
+                <Route path='test' element={<Projects />} />
             </Routes>
         </>
     )
